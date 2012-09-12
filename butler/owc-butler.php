cfce1edd556aa59362b87e0881def58e3a5d9f07
Jon McDonald <jon@onewebcentric.com>
2012-09-12 17:37:26 -0600

    Butler
    Adds the owc-butler to the repo

---
+++ butler/owc-butler.php
@@ -0,0 +1,20 @@
+<?php
+/*
+Plugin Name:  OWC Butler
+Plugin URI:  http://onewebcentric.com
+Description:  Adds some useful shortcode to WordPress
+Version:  .01
+Author URI:  http://onewebcentric.com
+Author:  Jon McDonald of OneWebCentric
+*/
+
+/*
+ * Define our plugin path
+ */
+define( 'OWC_PATH', plugin_dir_path(__FILE__) );
+
+//Includes code to create sliders
+include_once( OWC_PATH . '/slider.php');
+
+
+?>
